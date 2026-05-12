d10d68f9d8a0cb9aa247b1e9428b201e18b4d605
Abdul Wasay <abdulwasayrao917@gmail.com>
2026-05-12 12:37:45 +0500

    4th commit

--- translator/views.py
+++ translator/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from deep_translator import GoogleTranslator
 
-# Create your views here.
+def home(request):
+    translated = ""
+    text = ""
+
+    if request.method == "POST":
+        text = request.POST.get("text")
+        direction = request.POST.get("direction")
+
+        if direction == "en_ur":
+            translated = GoogleTranslator(source='en', target='ur').translate(text)
+
+        elif direction == "ur_en":
+            translated = GoogleTranslator(source='ur', target='en').translate(text)
+
+    return render(request, "translator/home.html", {
+        "translated": translated,
+        "text": text
+    })
