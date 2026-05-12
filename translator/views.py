496549c4d47aa0995a3f398a59e31c654fb1cdd3
Abdul Wasay <abdulwasayrao917@gmail.com>
2026-05-12 12:44:35 +0500

    6th commit

--- translator/views.py
+++ translator/views.py
@@ -1,21 +1,26 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 from deep_translator import GoogleTranslator
 
-def home(request):
-    translated = ""
-    text = ""
 
+def home(request):
+    return render(request, "translator/home.html")
+
+
+def translate_text(request):
     if request.method == "POST":
         text = request.POST.get("text")
         direction = request.POST.get("direction")
 
-        if direction == "en_ur":
-            translated = GoogleTranslator(source='en', target='ur').translate(text)
+        try:
+            if direction == "en_ur":
+                translated = GoogleTranslator(source='en', target='ur').translate(text)
+            else:
+                translated = GoogleTranslator(source='ur', target='en').translate(text)
 
-        elif direction == "ur_en":
-            translated = GoogleTranslator(source='ur', target='en').translate(text)
+            return JsonResponse({"translated": translated})
 
-    return render(request, "translator/home.html", {
-        "translated": translated,
-        "text": text
-    })
+        except Exception as e:
+            return JsonResponse({"error": str(e)})
+
+    return JsonResponse({"error": "Invalid request"})
